71bcf3c3dd5ba10d49a5dcd786803535e45d56c5
Lu <mluceropolo@gmail.com>
2021-12-24 15:07:32 -0300

    Agregué los archivos correspondientes al desafio 2

---
+++ Algoritmo condicional (desafio 2)/js/script.js
@@ -0,0 +1,8 @@
+
+let numeroIngresado = parseInt(prompt("Ingrese un número entre 10 y 50"));
+if (numeroIngresado >= 10 && numeroIngresado <= 50) {
+    alert ("Número ingresado correctamente: " +numeroIngresado);
+} else {
+    alert("Debe ingresar un número entre 10 y 50");
+}
+console.log("Siguiente");
